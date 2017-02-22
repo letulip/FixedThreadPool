b15bdf1734cb1d1d05ca27b3d72f552ba54225db
letulip <ivladimirskiy@ya.ru>
2017-02-22 10:37:49 +0300

    myThread класс добавлен
    В классе конструктор myThread получает имя функции и номер до кторого ведется счет.
    Функция func потока обрабатывает входные данные с Mythread и производит вывод в консоль.

---
+++ myThread.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FixedThreadPool
+{
+    class myThread
+    {
+        Thread thread;
+
+        public myThread(string name, int num) //Конструктор получает имя функции и номер до кторого ведется счет
+        {
+            thread = new Thread(this.func);
+            thread.Name = name;
+            thread.Start(num);//передача параметра в поток
+        }
+
+        void func(object num)//Функция потока, передаем параметр
+        {
+            for (int i = 0; i < (int)num; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + ": " + i);
+                Thread.Sleep(0);
+            }
+        }
+
+    }
+}
