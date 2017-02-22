2bd53df0df1e536e1c27920e8d1f6758844b62df
letulip <ivladimirskiy@ya.ru>
2017-02-22 10:28:24 +0300

    Приложение принимает и печатает входные параметры
    1. приложение принимает через аргументы два параметра mainCount и threadCount
    * целое число для основного потока
    * целое число для дополнительного потока
    2. печатает входящие параметры

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixedThreadPool
+{
+    class Program
+    {
+        static int mainCount;
+        static int threadCount;
+        
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length < 2)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Недостаточно входных данных");
+            }
+
+            mainCount = Convert.ToInt32(args[0]);
+            threadCount = Convert.ToInt32(args[1]);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("run.exe {0} {1}", mainCount, threadCount);
+
+            Console.ReadKey();
+        }
+    }
+}
